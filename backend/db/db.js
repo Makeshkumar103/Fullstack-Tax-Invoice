f163b5922e7403eaa94a8fefc8d40831ccf941da
Makeshkumar103 <makeshlg6@gmail.com>
2026-02-14 19:23:05 +0530

    db.js modifiyed

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 
 // Create MySQL connection
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
+// const db = mysql.createConnection(process.env.MYSQL_URL);
 
 
 // Connect to DB
