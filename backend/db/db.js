851885a83adb6e77c206adcc6880a9c712a09871
Makeshkumar103 <makeshlg6@gmail.com>
2026-02-14 17:27:29 +0530

    Backend ready for Railway

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 
 // Create MySQL connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+const db = mysql.createConnection(process.env.MYSQL_URL);
+
 
 // Connect to DB
 db.connect((err) => {
