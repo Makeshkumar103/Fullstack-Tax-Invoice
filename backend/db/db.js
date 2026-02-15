8f7b13e41fb68c1bd4bfd205b7ea2458ec987b50
Makeshkumar103 <makeshlg6@gmail.com>
2026-02-15 12:38:07 +0530

    fixed vercel build 2

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql2");
 
-// Create MySQL connection using Railway provided MYSQL_URL
-const db = mysql.createConnection(process.env.MYSQL_URL);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 
-// Connect to DB
 db.connect((err) => {
   if (err) {
     console.error("❌ Database connection failed:", err);
